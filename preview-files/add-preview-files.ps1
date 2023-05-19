5bbc887aaf56906695c0ad182e4300f39de3f06c
Mikhail Buinouski <m.buynovsky@syberry.com>
2023-05-19 04:13:49 +0300

    Use default colors in inasena, add docker support

--- preview-files/add-preview-files.ps1
+++
@@ -1,5 +0,0 @@
-# This command creates all files used for the preview
-New-Item 1.js
-New-Item 1.php
-New-Item 1.cs
-New-Item 1.java
--- preview-files/remove-preview-files.ps1
+++
@@ -1,5 +0,0 @@
-# This command removes all files used for the preview
-Remove-Item 1.js
-Remove-Item 1.php
-Remove-Item 1.cs
-Remove-Item 1.java
